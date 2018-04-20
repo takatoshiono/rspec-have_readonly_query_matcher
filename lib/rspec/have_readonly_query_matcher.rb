dc8181518b177fbf558d78e832a41f786288719d
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-04-21 02:12:48 +0900

    first implementation

--- lib/rspec/have_readonly_query_matcher.rb
+++ lib/rspec/have_readonly_query_matcher.rb
@@ -2,6 +2,30 @@ require "rspec/have_readonly_query_matcher/version"
 
 module RSpec
   module HaveReadonlyQueryMatcher
-    # Your code goes here...
+    def have_readonly_query
+      HaveReadonlyQuery.new
+    end
+
+    class HaveReadonlyQuery
+      def initialize
+      end
+
+      def matches?(given_proc)
+        given_proc.call
+        true
+      end
+
+      def failure_message
+        "expected have readonly queries"
+      end
+
+      def failure_message_when_negated
+        "expected not have readonly queries"
+      end
+
+      def supports_block_expectations?
+        true
+      end
+    end
   end
 end
--- spec/rspec/have_readonly_query_matcher_spec.rb
+++ spec/rspec/have_readonly_query_matcher_spec.rb
@@ -3,13 +3,40 @@ RSpec.describe RSpec::HaveReadonlyQueryMatcher do
     expect(RSpec::HaveReadonlyQueryMatcher::VERSION).not_to be nil
   end
 
+  module ::ActiveRecord
+    module ConnectionAdapters
+      class DummyAdapter
+        def execute(sql, name = nil)
+          { sql: sql, name: name }
+        end
+      end
+    end
+  end
+
+  let(:connection) { ::ActiveRecord::ConnectionAdapters::DummyAdapter.new }
+
   describe '#have_readonly_query' do
-    context 'when have readonly queries' do
-      it 'success'
+    context 'when have only read queries' do
+      subject do
+        connection.execute "DESCRIBE users", "NAME"
+        connection.execute "SELECT * FROM users", "NAME"
+      end
+
+      it 'success' do
+        expect { subject }.to have_readonly_query
+      end
     end
 
     context 'when have write queries' do
-      it 'fail'
+      subject do
+        connection.execute "DESCRIBE users", "NAME"
+        connection.execute "SELECT * FROM users", "NAME"
+        connection.execute "INSERT INTO users VALUES(1, 'foo')", "NAME"
+      end
+
+      it 'fail' do
+        expect { subject }.not_to have_readonly_query
+      end
     end
   end
 end
