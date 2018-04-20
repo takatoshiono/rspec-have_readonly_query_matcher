f6e90372b28cca1be74df090f9841fb09e351fc4
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-04-21 01:34:10 +0900

    Add spec skeleton

--- spec/rspec/have_readonly_query_matcher_spec.rb
+++ spec/rspec/have_readonly_query_matcher_spec.rb
@@ -3,7 +3,13 @@ RSpec.describe RSpec::HaveReadonlyQueryMatcher do
     expect(RSpec::HaveReadonlyQueryMatcher::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#have_readonly_query' do
+    context 'when have readonly queries' do
+      it 'success'
+    end
+
+    context 'when have write queries' do
+      it 'fail'
+    end
   end
 end
