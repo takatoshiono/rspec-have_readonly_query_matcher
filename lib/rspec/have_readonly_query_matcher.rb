7f52fc0575838445c3edbc8b71c07c0ac3438567
Takatoshi Ono <takatoshi.ono@gmail.com>
2018-04-21 01:07:47 +0900

    initial commit

---
+++ lib/rspec/have_readonly_query_matcher.rb
@@ -0,0 +1,7 @@
+require "rspec/have_readonly_query_matcher/version"
+
+module Rspec
+  module HaveReadonlyQueryMatcher
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/have_readonly_query_matcher/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module HaveReadonlyQueryMatcher
+    VERSION = "0.1.0"
+  end
+end
